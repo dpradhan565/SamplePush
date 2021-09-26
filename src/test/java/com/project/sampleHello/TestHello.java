a0b11d8150d812ea14060894446cbd264af42c2b
Dhaneshwar Pradhan <dhaneshwar.pradhan@cognizant.com>
2021-09-26 17:01:16 +0530

    Added Test

---
+++ src/test/java/com/project/sampleHello/TestHello.java
@@ -0,0 +1,17 @@
+package com.project.sampleHello;
+
+import org.junit.Test;
+
+import com.project.sampleGitHub.HelloWorld;
+
+
+
+public class TestHello {
+
+	HelloWorld helloWorld = new HelloWorld();
+	
+	@Test
+	public void testGetHelloWorld() {
+		
+	}
+}
