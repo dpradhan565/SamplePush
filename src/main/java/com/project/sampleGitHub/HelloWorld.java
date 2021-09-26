fc26464bf3751084caa425ef7492cfc633b8875d
Dhaneshwar Pradhan <dhaneshwar.pradhan@cognizant.com>
2021-09-26 16:40:13 +0530

    Initial Commit

---
+++ src/main/java/com/project/sampleGitHub/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.project.sampleGitHub;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.getHelloWorld();
+
+	}
+
+	public String getHelloWorld() {
+		return "Hello World";
+	}
+}
---
+++ src/test/java/com/project/sampleGitHub/TestHelloWorld.java
@@ -0,0 +1,15 @@
+package com.project.sampleGitHub;
+
+import org.junit.Test;
+
+
+
+public class TestHelloWorld  {
+
+	HelloWorld helloWorld = new HelloWorld();
+	
+	@Test
+	public void testGetHelloWorld() {
+		
+	}
+}
