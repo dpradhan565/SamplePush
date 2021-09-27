cad6bcc49ad40ff8f4f3682179e5e306327c117b
Dhaneshwar Pradhan <dhaneshwar.pradhan@cognizant.com>
2021-09-27 06:47:14 +0530

    changed

---
+++ src/test/java/com/project/sampleTest/TestHello.java
@@ -0,0 +1,15 @@
+package com.project.sampleTest;
+
+import org.junit.Test;
+
+import com.project.sampleGitHub.HelloWorld;
+
+public class TestHello {
+HelloWorld helloWorld = new HelloWorld();
+	
+	@Test
+	public void testGetHelloWorld() {
+		
+	}
+
+}
